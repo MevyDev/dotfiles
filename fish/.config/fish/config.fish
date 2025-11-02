9221b2db5a852f4330bb614a0029b7876bdade55
MevyDev <mevydev@proton.me>
2025-11-02 13:00:29 +0100

    yazi helper function

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -10,4 +10,13 @@ end
 
 alias vim nvim
 
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+
 starship init fish | source
